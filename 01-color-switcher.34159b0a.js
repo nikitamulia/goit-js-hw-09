9ff10206ba67577477bad84f50dc2cb395596df0
nikitamulia <111284064+nikitamulia@users.noreply.github.com>
2022-10-26 20:23:06 +0000

    Deploying to gh-pages from @ nikitamulia/goit-js-hw-09@2f0fcc23f2b553177f0ecf614ae07cff8205ef45 🚀

---
+++ 01-color-switcher.34159b0a.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");t.addEventListener("click",(function(){timerId=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`,t.disabled=!0}),1e3)})),e.addEventListener("click",(function(){clearInterval(timerId),t.disabled=!1}));
+//# sourceMappingURL=01-color-switcher.34159b0a.js.map
---
+++ 01-color-switcher.3d465a2c.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");t.addEventListener("click",(function(){timerId=setInterval((function(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16)),t.disabled=!0}),1e3)})),e.addEventListener("click",(function(){clearInterval(timerId),t.disabled=!1}))}();
+//# sourceMappingURL=01-color-switcher.3d465a2c.js.map
