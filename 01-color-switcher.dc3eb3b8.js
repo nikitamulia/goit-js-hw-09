aa7779b0376fc4b20c43ec316d3d0635ed0d89a5
nikitamulia <111284064+nikitamulia@users.noreply.github.com>
2022-10-28 20:06:25 +0000

    Deploying to gh-pages from @ nikitamulia/goit-js-hw-09@fff3523b6161fff7f7b3dff8041b9206d9a9cc99 🚀

--- 01-color-switcher.dc3eb3b8.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");t.addEventListener("click",(function(){n=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`,t.disabled=!0}),1e3)})),e.addEventListener("click",(function(){clearInterval(n),t.disabled=!1}));let n=null;
-//# sourceMappingURL=01-color-switcher.dc3eb3b8.js.map
